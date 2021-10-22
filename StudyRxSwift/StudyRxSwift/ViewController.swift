c59bc390ea90c4313419edb95d8718311f0aec3b
Egor Gorskikh <egorgorskikh@gmail.com>
2021-10-22 19:24:02 +0300

    init: RxSwift

---
+++ StudyRxSwift/StudyRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyRxSwift
+//
+//  Created by Egor Gorskikh on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
